8b54dc52a5a95fe2dbfc2c679d6fffacf5edccb6
Cindy Singer <cindy_singer913@msn.com>
2024-01-09 14:42:10 +0100

    j

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-# premier programme
-
 # connexion à la base de données
+import mysql.connector
+
+# Remplacez les valeurs suivantes par les informations de votre propre base de données
+host = "votre_host"
+user = "votre_utilisateur"
+password = "votre_mot_de_passe"
+database = "votre_base_de_donnees"
+
+# Établir la connexion à la base de données
+connexion = mysql.connector.connect(
+    host=host, user=user, password=password, database=database
+)
+
+# Verifier si la connexion a réussi
+if connexion.is_connected():
+    print("Connecté à la base de données")
+else:
+    print("Échec de la connexion à la base de données")
+
+# Fermer la connexion
+connexion.close()
